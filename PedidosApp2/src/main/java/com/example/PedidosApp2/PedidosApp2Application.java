a5995cfc4ca058a00e63abbba59dcca2f8daa56c
Felip3 <juanfeliperamirezcardona0@gmail.com>
2025-01-31 15:43:09 -0500

    pedidos2

---
+++ PedidosApp2/src/main/java/com/example/PedidosApp2/PedidosApp2Application.java
@@ -0,0 +1,13 @@
+package com.example.PedidosApp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosApp2Application.class, args);
+	}
+
+}
---
+++ PedidosApp2/src/test/java/com/example/PedidosApp2/PedidosApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PedidosApp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosApp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
